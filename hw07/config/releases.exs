0e2eed9427233103ab5613fda6901897b18b2694
chy101010 <jason2028823@gmail.com>
2021-03-05 09:48:48 -0500

    deployment

--- hw07/config/releases.exs
+++ hw07/config/releases.exs
@@ -26,18 +26,17 @@ secret_key_base =
 
 config :hw07, Hw07Web.Endpoint,
   http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
+    port: String.to_integer(System.get_env("PORT") || "4082"),
     transport_options: [socket_opts: [:inet6]]
   ],
-  secret_key_base: secret_key_base,
-  server: true
+  secret_key_base: secret_key_base
 
 # ## Using releases (Elixir v1.9+)
 #
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :hw07, Hw07Web.Endpoint, server: true
+config :hw07, Hw07Web.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
